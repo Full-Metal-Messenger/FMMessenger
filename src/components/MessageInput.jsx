5237baf94d697aa9e6cd644a0fcde5d11faab0a6
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-27 11:27:53 -0700

    added MessageInput

---
+++ src/components/MessageInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MessageInput() {
+
+  return (
+    <div>
+    <form>
+        <input type='text' value={post}/> 
+        <button>Send</button>
+    </form>
+
+    </div>
+  )
+}
+
+export default MessageInput
