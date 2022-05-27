d4e0c65019aa3d271f14a8ff77b51d00fedc9508
BrendenSeifried <brendenseifried@gmail.com>
2022-05-27 12:55:34 -0700

    for bradley via chackra

--- src/hooks/UseMessage/UseMessage.js
+++ src/hooks/UseMessage/UseMessage.js
@@ -1,34 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useAuthContext } from '../../context/AuthContext';
-import { getMessages, subscribe, unsubscribe } from '../../services/fetchMessages';
+import { getMessages, subscribe, unsubscribe } from '../../services/messages';
 
 function useMessage() {
-    const [messages, setMessages] = useState([]);
-    const {user} = useAuthContext();
-    console.log(user);
-    const [post, setPost] = useState('')
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        await postMessage(post, user.id);
-        setPost('');
-    }
-    const handleMessageReceived = (message) => {
-        setMessages((prevMessages) => [...prevMessages, message]);
-      };
-    
-    useEffect(() => {
-        getMessages().then(setMessages);
-    
-        subscribe(handleMessageReceived);
-    
-        return () => unsubscribe();
-      }, []);
-    
+  const [messages, setMessages] = useState([]);
+  const { user } = useAuthContext();
+  console.log(user);
+  const [post, setPost] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await postMessage(post, user.id);
+    setPost('');
+  };
+  const handleMessageReceived = (message) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  };
 
-  return {handleSubmit, messages, setPost, handleMessageReceived, post}
+  useEffect(() => {
+    getMessages().then(setMessages);
 
-   
-  
+    subscribe(handleMessageReceived);
+
+    return () => unsubscribe();
+  }, []);
+
+  return { handleSubmit, messages, setPost, handleMessageReceived, post };
 }
 
-export default useMessage
+export default useMessage;
