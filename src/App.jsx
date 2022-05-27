6a9bc4bbb8ab7ae16f54a21437228f40f634ac8b
Bradley-Bird <Bradleybird2@icloud.com>
2022-05-27 14:46:33 -0700

    messsage context created , posts now pushing correctly to supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import Main from './views/Main';
 import Auth from './views/Auth';
 import { Route, Switch } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Switch>
-        <Route path="/auth">
-          <Auth />
-        </Route>
-        <Route path="/">
-          <Main />
-        </Route>
-      </Switch>
-    </AuthProvider>
+    <Switch>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+      <Route path="/">
+        <Main />
+      </Route>
+    </Switch>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import { render } from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { MessageProvider } from './context/MessageContext';
 
 render(
   <React.StrictMode>
-    <Router>
-      <ChakraProvider resetCSS={true}>
-        <App />
-      </ChakraProvider>
-    </Router>
+    <ChakraProvider resetCSS={true}>
+      <AuthProvider>
+        <MessageProvider>
+          <Router>
+            <App />
+          </Router>
+        </MessageProvider>
+      </AuthProvider>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
