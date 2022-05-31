e760401f88160442749854f4f846f3612a8c6704
BrendenSeifried <brendenseifried@gmail.com>
2022-05-31 10:59:08 -0700

    got logout button working but still need to clear parameters of inputs in singin signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import Main from './views/Main';
 import Auth from './views/Auth';
 import { Route, Switch } from 'react-router-dom';
 import { PrivateRoute } from './components/PrivateRoute';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/auth">
-        <Auth />
-      </Route>
-      <PrivateRoute path="/">
-        <Main />
-      </PrivateRoute>
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <PrivateRoute path="/">
+          <Main />
+        </PrivateRoute>
+      </Switch>
+    </>
   );
 }
