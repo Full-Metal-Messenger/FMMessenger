790527ef40af9ae5023cf37a44f6b63c4f60b7c0
BrendenSeifried <brendenseifried@gmail.com>
2022-06-01 11:28:07 -0700

    added incons to toggle light mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,6 +14,7 @@ import {
   useColorMode,
   useDisclosure,
 } from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
@@ -21,9 +22,11 @@ import useRooms from '../hooks/useRooms';
 import { logout } from '../services/auth';
 import NewRoomPop from './NewRoomPop';
 import Profiles from './Profiles';
+import { useState } from 'react';
 
 export default function Header() {
   const { toggleColorMode } = useColorMode();
+  const [light, setLight] = useState(true);
 
   const history = useHistory();
   const { user, setCurrentUser, setEmail, setPassword, setusername } =
@@ -56,8 +59,13 @@ export default function Header() {
           LogOut
         </Button>
       )}
-      <Button m="5" onClick={toggleColorMode}>
-        Toggle
+      <Button
+        m="5"
+        onClick={() => {
+          setLight(!light), toggleColorMode();
+        }}
+      >
+        {light ? <FaMoon /> : <FaSun />}
       </Button>
       <NewRoomPop />
     </Flex>
