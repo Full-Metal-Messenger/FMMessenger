1bf1aa60b3c8b97dc2b91b24bf0f362d8f37e80f
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-01 16:19:54 -0700

    for pull dev

--- src/services/rooms.js
+++ src/services/rooms.js
@@ -2,6 +2,7 @@ import { client, parseData } from './client';
 
 export async function createRoom(name) {
   //calling a function that we make on supabase
-  const resp = await client.rpc('create_room', { name: name });
+  const resp = await client.rpc('create_room', { name: name }).single();
+  console.log('respCreate', resp);
   return parseData(resp);
 }
