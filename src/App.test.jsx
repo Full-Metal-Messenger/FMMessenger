d7049edac5d2ca87361f032552a79ab39d8d5104
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-01 13:08:51 -0700

    moved closing brackets to after const

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,23 +1,25 @@
-import { screen } from '@testing-library/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { render, screen } from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { MessageProvider } from './context/MessageContext';
 
 const Mock = {
-  results: [
-    {
-      id: 1,
-      username: 'Bill',
-      // created_at: 01 / 21 / 2022,
-      posts: 'Nice',
-      profile_id: 2,
-    },
-  ],
+  results: {
+    id: 1,
+    username: 'Bill',
+    // created_at: 01 / 21 / 2022,
+    posts: 'Nice',
+    profile_id: 2,
+  },
 };
 
 const server = setupServer(
-  rest.post(
-    'https://kplqqqfafshaldfzhgir.supabase.co/auth/v1/token?grant_type=refresh_token',
-    (req, res, ctx) => res(ctx.json(Mock))
+  rest.get(`${process.env.SUPABASE_URL}/auth`, (req, res, ctx) =>
+    res(ctx.json(Mock))
   )
 );
 
@@ -25,10 +27,29 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-describe('example test', () => {
-  it('first test', () => {
-    return;
+describe('Login Test', () => {
+  it('Log user', async () => {
+    render(
+      <ChakraProvider>
+        <AuthProvider>
+          <MessageProvider>
+            <MemoryRouter>
+              <App />
+            </MemoryRouter>
+          </MessageProvider>
+        </AuthProvider>
+      </ChakraProvider>
+    );
+
+    const emails = await screen.findByPlaceholderText('user email');
+    expect(emails).toBeInTheDocument();
     screen.debug();
   });
 });
-//test file
+
+// describe('example test', () => {
+//   it('first test', () => {
+//     return;
+//   });
+// });
+// //test file
